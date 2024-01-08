404b00332c9a31608da87bc20e78653f72b53de1
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-07 23:22:06 -0600

    uppercase char style

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,6 @@ def uppercase(s):
     for char in s:
         if 'a' <= char <= 'z':
             char = chr(ord(char) - 32)
-        print(char, end="")
+        print("{}".format(char), end="")
     print()
+    
