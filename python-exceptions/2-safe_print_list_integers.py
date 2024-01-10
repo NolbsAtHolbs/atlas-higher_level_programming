705a54920bd8ae3a65d680a0ea44089d3fbaffdf
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-09 23:01:42 -0600

    int printer limit

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    try:
+        for elementx in range(x):
+            print("{:d}".format(my_list[elementx]), end=" ")
+            count += 1
+        print()
+    except (ValueError, TypeError, IndexError):
+        pass
+    return count
