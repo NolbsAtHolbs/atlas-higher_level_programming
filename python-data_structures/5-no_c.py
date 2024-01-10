ba43dbeb1bb630d9ae73296d0091993faf76a8ee
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-09 20:15:53 -0600

    reversed integer list printer perms retry

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(list=[]):
-    for i in range(len(list) - 1, -1, -1):
-        print("{:d}".format(list[i]))
+    for num in reversed(list):
+        print("{:d}".format(num))
