d3bec80dff96326c669c9f5fe01166b36ed21990
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-09 20:38:51 -0600

    negative case check

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,9 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for num in row:
-            print("{:d}".format(num))
+        for i in range(len(row)):
+            if i == len(row) - 1:
+                print("{:d}".format(row[i]), end="")
+            else:
+                print("{:d}".format(row[i]), end=" ")
+        print("")
