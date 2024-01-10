b95cadcf05288ca2788450b79e52d0e0b1b73238
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-09 23:08:21 -0600

    error

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,7 +3,11 @@
 def safe_print_division(a, b):
     try:
         result = a / b
+    except ZeroDivisionError:
+        result = None
+    except Exception as e:
+        print("Exception:", e)
+        return None
+    finally:
         print("Inside result: {}".format(result))
         return result
-    except ZeroDivisionError:
-        return None
