c684dde184f21d20427ba4bb66c0549ecc205056
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-09 23:35:24 -0600

    min + list len test

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    count = 0
-    try:
-        for number in range(x):
-            print(my_list[number], end="")
-            count += 1
-    except IndexError:
-        pass
-    print("")
-    return count
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+        except IndexError:
+            break
+    print()
+    return min(x, len(my_list))
