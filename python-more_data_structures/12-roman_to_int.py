69cc248daaaa4f270f51b942446afa2b3f42c1db
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-09 22:36:44 -0600

    print any elements by x arg from list of any type

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
 
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str):
+        return 0
+    key_mapping = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    total = 0
+    for i in range(len(roman_string)):
+        if i > 0 and key_mapping[roman_string[i]] > key_mapping[roman_string[i-1]]:
+            total += key_mapping[roman_string[i]] - 2 * key_mapping[roman_string[i-1]]
+        else:
+            total += key_mapping[roman_string[i]]
+    return total
