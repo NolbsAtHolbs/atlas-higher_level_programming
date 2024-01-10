12734b26d6cc9cae42d73885cf3d1d88be00255d
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-09 22:55:58 -0600

    element printer

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,12 +2,11 @@
 
 def safe_print_list(my_list=[], x=0):
     count = 0
-    for item in my_list[:x]:
-        try:
-            print(item, end=" ")
-            count += 1
-        except Exception as e:
-            print("\nError:", e)
-            break
+    try:
+        for element in range(x):
+            print(my_list[element], end="")
+            count = count + 1
+    except IndexError:
+        pass
     print("")
     return count
