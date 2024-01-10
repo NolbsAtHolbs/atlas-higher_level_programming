5d49b12c12a9d9aee6e25272d76777ea25d099a9
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-09 20:43:16 -0600

    none case check

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
-def print_matrix_integer(matrix=[[]]):
+def print_matrix_integer(matrix=None):
+    if matrix is None:
+        print("Matrix is None")
+        return
+
     for row in matrix:
         for i in range(len(row)):
             if i == len(row) - 1:
