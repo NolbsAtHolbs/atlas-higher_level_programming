6cc0e66b3023c41cfe55fcd4ceb9a2d36b6f12d6
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-09 23:30:28 -0600

    x += 1 matters

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,9 +3,9 @@
 def safe_print_list(my_list=[], x=0):
     count = 0
     try:
-        for element in range(x):
-            print(my_list[element], end="")
-            count = count + 1
+        for number in range(x):
+            print(my_list[number], end="")
+            count += 1
     except IndexError:
         pass
     print("")
