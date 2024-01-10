a1cd236d48ba8c7775fe2ae580437b3918acf3a6
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-09 20:49:05 -0600

    safe tuple test

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    max_length = max(len(tuple_a), len(tuple_b))
-    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]) if max_length > 1 else (tuple_a[0] + tuple_b[0], 0)
+    safe_tuple_a = tuple_a + (0, 0)[:max(0, 2 - len(tuple_a))]
+    safe_tuple_b = tuple_b + (0, 0)[:max(0, 2 - len(tuple_b))]
+
+    result_tuple = (safe_tuple_a[0] + safe_tuple_b[0], safe_tuple_a[1] + safe_tuple_b[1])
+
+    return result_tuple
