ebf1e16df0ef934c3fe8f5db899403a4c9a3bf40
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-09 20:46:03 -0600

    beta tuple test

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
 
+def add_tuple(tuple_a=(), tuple_b=()):
+    max_length = max(len(tuple_a), len(tuple_b))
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]) if max_length > 1 else (tuple_a[0] + tuple_b[0], 0)
