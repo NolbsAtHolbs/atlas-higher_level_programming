4d0c33f62334313ea19f35522dde280525a12d09
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-09 23:45:51 -0600

    redone whole file

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    index = 0
-    for i in range(x):
-        try:
-            if i < len(my_list):
-                print("{:d}".format(my_list[i]), end="")
-                index += 1
-        except TypeError:
-            print("")
-            return index
-        except IndexError:
-            print("")
-            return index
-    print("")
-    return index
+    try:
+        for i in range(x):
+            print("{:d}".format(my_list[i]), end="")
+    except IndexError:
+        print("\n")
+        return i
+    else:
+        print("\n")
+        return i + 1
