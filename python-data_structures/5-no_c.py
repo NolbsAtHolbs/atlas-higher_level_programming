c3aa50d3fcd0f1786f0e9127293998e15277726d
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-09 20:14:46 -0600

    reversed integer list printer 2

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(list=[]):
-    for num in reversed(list):
-        print("{:d}".format(num))
+    for i in range(len(list) - 1, -1, -1):
+        print("{:d}".format(list[i]))
