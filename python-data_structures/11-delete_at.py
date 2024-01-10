5b957838cdcfaa2dedae35e0a1e396fe33c3d66f
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-09 21:03:32 -0600

    update list for return

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
-    if idx >= 0 and idx < len(my_list):
-        return my_list[:idx] + my_list[idx+1:]
-    else:
+    if idx < 0 or idx >= len(my_list):
         return my_list
+    else:
+        my_updated_list = my_list[:idx] + my_list[idx + 1:]
+        return my_updated_list
