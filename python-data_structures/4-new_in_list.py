51511369784b9cb5f25a32d7ae41c30b5dc61c6e
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-09 20:21:46 -0600

    i can redefine prototypes?

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+
+def new_in_list(list, index, element):
+    if index < 0 or index >= len(list):
+        return list.copy()
+    else:
+        return list[:index] + [element] + list[index+1:]
