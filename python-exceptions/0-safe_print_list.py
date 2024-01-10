183883c04c6f963820670963e689605d800785b6
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-09 23:41:54 -0600

    nvm cant use len

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+    index = 0
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]), end="")
+            if i < len(my_list):
+                print("{:d}".format(my_list[i]), end="")
+                index += 1
+        except TypeError:
+            print("")
+            return index
         except IndexError:
-            break
+            print("")
+            return index
     print("")
-    return min(x, len(my_list))
+    return index
