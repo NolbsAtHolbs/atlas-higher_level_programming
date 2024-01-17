431f3935849b0a083a0fc40da74a471d05b729bd
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-16 23:24:46 -0600

    new lines testify

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""Module for function that prints text with 2 new lines after ., ?, and :"""
+
+
+def text_indentation(text):
+    
+    """Function that prints text with 2 lines after ., ?, and :"""
+    
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    chars_to_split = [".", "?", ":"]
+    start = 0
+    for i, char in enumerate(text):
+        if char in chars_to_split:
+            if text[i + 1] == " ":
+                print(text[start:i + 1].strip())
+            else:
+                print(text[start:i + 1])
+            print("")
+            start = i + 2
+
+    if start < len(text):
+        print(text[start:].strip())
