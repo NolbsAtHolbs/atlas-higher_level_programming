e838447a0ca3bafbd0a019be6039a5ee6f899a39
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-16 19:51:56 -0600

    square defined by size

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""This is module documentation for a Square class"""
+
+
+class Square:
+    """This is the Square class documentation"""
+    def __init__(self, size):
+        self.__size = size
