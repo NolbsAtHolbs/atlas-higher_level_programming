23ed70a19fef9a07b104bf2bbeac9f3f441e2d52
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-16 23:18:14 -0600

    square printer testify

--- python-test_driven_development/4-print-square.py
+++ python-test_driven_development/4-print-square.py
@@ -1,10 +1,15 @@
+#!/usr/bin/python3
+
+"""This is a module for a function that prints a square with the char #"""
+
+
 def print_square(size):
+    """This function prints a square with the character #"""
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
     if isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
-
-    for _ in range(size):
+    for z in range(size):
         print("#" * size)
