9572eb29b28bde3ce7f2f816caf2ceebadc3bcf7
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-16 21:27:57 -0600

    eval?

---
+++ python-more_classes/4-rectangle.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+"""This is module documentation"""
+
+
+class Rectangle:
+    """This is class documentation for a rectangle"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width > 0 and self.__height > 0:
+            return 2 * (self.__width + self.__height)
+        else:
+            return 0
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ("")
+        else:
+            return ("\n".join(["#" * self.__width
+                    for y in range(self.__height)]))
+
+    def __repr__(self):
+        return f"Rectangle({self.__width}, {self.__height})"
+    
