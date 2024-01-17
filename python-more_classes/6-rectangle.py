4b901cdf3ee990ba3c96c0206c50bd1611d02526
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-16 21:42:04 -0600

    define num of inst

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -5,6 +5,8 @@
 
 class Rectangle:
     """This is class documentation for a rectangle"""
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
