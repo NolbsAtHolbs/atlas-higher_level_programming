33b8ed0ce6f04d3ad93d891efd761a005fed6028
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-16 23:50:06 -0600

    test test

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Module for a Unittest for max_integer function"""
+
+
+import unittest
+max_integer = __import__("6-max_integer").max_integer
+
+class UTMaxInteger(unittest.TestCase):
+	"""class unit tester"""
