2b7bb95692b4c43aed18e61051dede282bcdb037
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-17 23:18:39 -0600

    testing

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -2,11 +2,14 @@
 
 """This is a module for a function that divides all elements of a matrix"""
 
+
 def matrix_divided(matrix, div):
     """This is a function that divides all elements of a matrix"""
     if not (all(isinstance(row, list) for row in matrix)
-        or not all(all(isinstance(element, (int, float)) for element in row) for row in matrix)):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        or not all(all(isinstance(element, (int, float))
+            for element in row) for row in matrix)):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
     if not isinstance(div, (int, float)):
