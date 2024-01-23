c4582d39301770064474073cfe1e9b4d64b85053
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-23 17:50:23 -0600

    pcs

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -3,6 +3,7 @@
 """Module for function that returns the list of
     available attributes and methods of an object"""
 
+
 def lookup(obj):
     """(Method) function that looks up an object and returns the list of
         available attributes and methods of an object"""
