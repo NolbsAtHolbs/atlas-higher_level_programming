f73c73d010433f4e1d29dbfb1ddb66fc4a12af9d
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-23 22:10:36 -0600

    documentation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,5 +4,6 @@
 
 
 def read_file(filename=""):
+    """function that reads UTF8 text file and prints it to stdout"""
     with open(filename, encoding="utf-8") as file:
         print(file.read(), end="")
