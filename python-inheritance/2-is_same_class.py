779f36127b8ce2f0c1f9ddbf9d49b98c6d344e8b
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-23 18:07:55 -0600

    cool function

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Module for function that returns True if the object
+    is exactly an instance of the specified class, otherwise False"""
+
+
+def is_same_class(obj, a_class):
+    """Method function that returns True if the object
+        is exactly an instance of the specified class, otherwise False"""
+    if type(obj):
+        isinstance(a_class)
+        return True
+    else:
+        return False
