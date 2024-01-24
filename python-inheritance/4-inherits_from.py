20f0da87b5dfbe92be73a49631099d346896a65e
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-23 19:09:11 -0600

    issubclass for inherited specific subclass

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,6 +5,6 @@ inherited (directly or indirectly) from specified class, otherwise False"""
 
 
 def inherits_from(obj, a_class):
-	"""Function that checks if object is an inherited instance of a class"""
-	if type(obj) is not a_class:
+    """Function that checks if object is an inherited instance of a class"""
+    if type(obj) is not a_class:
         return issubclass(type(obj), a_class)
