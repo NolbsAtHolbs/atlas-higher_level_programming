0c90becf1d98cdcb5074554a5bc0d5652e3fec9f
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-23 23:01:39 -0600

    triangle math

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
 
-"""Module for """
+"""Module for function that returns list of ints for Pascal's triangle"""
 
 
+def pascal_triangle(n):
+    """Function that returns list of ints for Pascal's triangle"""
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
