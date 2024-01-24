d3b6ae39c4b1f716f9cf2aa02a158105e0605d13
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-23 22:38:21 -0600

    os json sys multi exec

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,10 +3,16 @@
 """Module for script that adds all arguments to a Python list,
 and then save them to a file"""
 
-import sys
+import os
 import json
+import sys
 
-arguments = sys.argv[1:]
+if not os.path.isfile('add_item.json'):
+    data = [sys.argv[1]]
+else:
+    with open('add_item.json', 'r') as file:
+        data = json.load(file)
+    data.append(sys.argv[1])
 
 with open('add_item.json', 'w') as file:
-    json.dump(arguments, file)
+    json.dump(data, file)
