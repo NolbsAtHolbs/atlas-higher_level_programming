1bb38fccf86d9dabda2dc62cfcb0f27d8114ae40
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-23 19:23:55 -0600

    empty class

--- python-inheritance/6-base-geometry.py
+++ python-inheritance/6-base-geometry.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+
+"""Module for class that raises a development exception"""
+
+
+class BaseGeometry:
+    """Class that raises Exception with message `area() is not implemented`"""
+    def area(self):
+        raise Exception('area() is not implemented')
