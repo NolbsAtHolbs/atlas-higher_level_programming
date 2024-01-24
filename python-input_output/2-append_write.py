919e82c455f67ab25164f46ba3c75bb7cad3f9f0
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-23 22:19:41 -0600

    append similarly with a

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
 
+"""Module for function that appends a string at the end of a UTF8 text file
+and returns the num of chars added"""
+
+
+def append_write(filename="", text=""):
+    """Function that appends a string at the end of a UTF8 text file
+    and returns the num of chars added"""
+    with open(filename, "a", encoding="utf-8") as file:
+        return file.write(text)
