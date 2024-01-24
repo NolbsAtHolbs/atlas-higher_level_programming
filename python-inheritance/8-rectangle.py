758ba083ff5cfc3ff0a3cbefac5dcbeba45b213a
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-23 20:24:23 -0600

    order self.__ carefully and validate after

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -3,3 +3,12 @@
 """"Module for class that raises a development exception & validates value and"""
 
 
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """Class for inherited rectangle"""
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", self.__width)
+        self.integer_validator("height", self.__height)
