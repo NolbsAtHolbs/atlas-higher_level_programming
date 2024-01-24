0389d5d07c30511b5d8e6d6616c9546407c11a91
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-23 22:35:13 -0600

    json + sys references

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
 
+"""Module for script that adds all arguments to a Python list,
+and then save them to a file"""
+
+import sys
+import json
+
+arguments = sys.argv[1:]
+
+with open('add_item.json', 'w') as file:
+    json.dump(arguments, file)
