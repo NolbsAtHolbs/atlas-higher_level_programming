5414447d280d22a68db5094aadc71fa77d945153
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-23 22:39:30 -0600

    undo

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,16 +3,10 @@
 """Module for script that adds all arguments to a Python list,
 and then save them to a file"""
 
-import os
-import json
 import sys
+import json
 
-if not os.path.isfile('add_item.json'):
-    data = [sys.argv[1]]
-else:
-    with open('add_item.json', 'r') as file:
-        data = json.load(file)
-    data.append(sys.argv[1])
+arguments = sys.argv[1:]
 
 with open('add_item.json', 'w') as file:
-    json.dump(data, file)
+    json.dump(arguments, file)
