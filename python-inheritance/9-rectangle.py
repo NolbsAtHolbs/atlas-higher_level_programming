a582edb41fcf124df450775004192fc11ac27618
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-23 20:41:18 -0600

    added area and magic method __str__ return descriptions

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+
+"""Module for class that inherits from prev files
+and adds area, print, and str"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Class for inherited rectangle"""
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", self.__width)
+        self.integer_validator("height", self.__height)
+
+    def area(self):
+        return self.__width * self.__height
+
+    def ___str___(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
