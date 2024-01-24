7064447eb721e3a5c289a0e643fb98614a2b27bc
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-23 20:03:20 -0600

    compact list print sort

--- python-inheritance/1-my-list.py
+++ python-inheritance/1-my-list.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+"""Module for class `MyList` that inherits
+a sorted printed list of ints from `list`"""
+
+
+class MyList(list):
+    """Class that will inherit from list"""
+    def print_sorted(self):
+        print(sorted(self))
