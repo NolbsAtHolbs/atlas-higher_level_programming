ef75d009543121e6e9a83ed5fadae45c56a25235
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-23 22:29:53 -0600

    json object create

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
 
+"""Module for function that creates an object from a JSON file"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """function that creates an object from a JSON file"""
+    with open(filename, "r") as file:
+        return json.load(file)
