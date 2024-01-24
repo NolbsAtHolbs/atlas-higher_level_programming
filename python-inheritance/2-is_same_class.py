c77511d98baad5721a29be8e988edde5e3070ec9
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-23 18:14:38 -0600

    test cooler function

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -9,5 +9,5 @@ def is_same_class(obj, a_class):
         is exactly an instance of the specified class, otherwise False"""
     if type(obj) is a_class:
         return True
-    else
+    else:
         return False
