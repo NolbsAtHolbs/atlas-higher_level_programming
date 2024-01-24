579e9738462a7405c0e61c502b9ca3ad3eff9414
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-23 22:22:05 -0600

    json conversion rep

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 
+"""Module for function that returns the JSON representation of a string object"""
+
+
+import json
+
+def to_json_string(my_obj):
+    """Function that returns the JSON representation of a string object"""
+    return json.dumps(my_obj)
