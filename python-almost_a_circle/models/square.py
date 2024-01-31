f038206adea0ef51a38b5de9c63bb80984d49eaa
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-30 21:05:49 -0600

    test

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -26,11 +26,11 @@ class Square(Rectangle):
     def update(self, *args, **kwargs):
         """Method that assigns key/value arguments to attributes"""
         if args:
-            if len(args) == 1:
+            if len(args) >= 1:
                 self.id = args[0]
-            if len(args) == 2:
+            if len(args) >= 2:
                 self.size = args[1]
-            if len(args) == 3:
+            if len(args) >= 3:
                 self.x = args[2]
             if len(args) == 4:
                 self.y = args[3]
