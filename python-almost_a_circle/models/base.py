d38b11058a5a90a948be607e5acae5b901b24b1a
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-30 21:32:36 -0600

    json string representation file writing

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -26,9 +26,9 @@ class Base:
     @classmethod
     def save_to_file(cls, list_objs):
         """Class method that writes JSON string representation of l_o to file"""
-        filename = cls.__name__ + ".json"
         if list_objs is None:
             list_objs = []
-        json_str = cls.to_json_string(list_objs)
-        with open(filename, 'w') as f:
-            f.write(json_str)
+        filename = f"{cls.__name__}.json"
+        with open(filename, "w") as file:
+            json_string = cls.to_json_string([obj.to_dictionary() for obj in list_objs])
+            file.write(json_string)
