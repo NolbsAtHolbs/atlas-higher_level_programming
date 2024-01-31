07a67ef91f2b73b76b5a92905b23c08a5969ff48
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-30 19:54:12 -0600

    update str method

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -74,4 +74,4 @@ class Rectangle(Base):
     def __str__(self):
         """Class update overriding the str method"""
         return f"[Rectangle] ({self.id})
-                {self.x}/{self.y} - {self.width}/{self.height}"
+                {self.__x}/{self.__y} - {self.__width}/{self.__height}"
