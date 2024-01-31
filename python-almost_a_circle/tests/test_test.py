4ee079b975a66acd1dccea7f3e61fd60d9241f61
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-30 22:33:26 -0600

    tests

--- python-almost_a_circle/tests/test_test.py
+++ python-almost_a_circle/tests/test_test.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+
+"""Module for unit testing and PEP8 validation"""
+
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+import sys
+import os
+import json
+import io
+
+
+class TestBase(unittest.TestCase):
+    """Base class tests"""
+    def baseid(self):
+        """test do it test done"""
+        b1 = Base()
+        b2 = Base()
+        b3 = Base(8)
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
+        self.assertEqual(b3.id, 8)
