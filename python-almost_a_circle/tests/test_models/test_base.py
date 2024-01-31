5e9bf7d90ca2a7ee700a700ab5a412641f302200
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-30 23:49:36 -0600

    tests test

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,53 +1,50 @@
 #!/usr/bin/python3
 
-"""Module for unit testing and PEP8 validation"""
-
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
-import sys
 from io import StringIO
+import sys
 
 
-class TestBase(unittest.TestCase):
-    """Base class tests"""
-    def baseid(self):
+class TestBaseAttributes(unittest.TestCase):
+    def test_id_assignment(self):
         self.assertEqual(Base._Base__nb_objects, 0)
         test_obj = Base()
         self.assertEqual(test_obj.id, 1)
 
-    def testid(self):
+    def test_custom_id(self):
         id_obj = Base(89)
         self.assertEqual(id_obj.id, 89)
 
-class TestJSON(unittest.TestCase):
-    def empty(self):
+class TestJSONStringMethod(unittest.TestCase):
+    def test_empty(self):
         self.assertEqual(Base.to_json_string(None), "[]")
 
-    def notdict(self):
-        self.assertEqual(Base.to_json_string([3, 4, 5]), "[3, 4, 5]")
+    def test_non_dict(self):
+        self.assertEqual(Base.to_json_string([1, 2, 3]), "[1, 2, 3]")
 
-    def notlist(self):
-        self.assertEqual(Base.to_json_string(51), "51")
+    def test_non_list(self):
+        self.assertEqual(Base.to_json_string(42), "42")
 
-    def success(self):
-        r1 = Rectangle(5, 7)
-        dict1 = r1.to_dictionary()
-        check = '[{"id": 2, "width": 5, "height": 7, "x": 0, "y": 0}]'
-        self.assertEqual(Base.to_json_string([dict1]), check)
+    def test_success(self):
+        rect_1 = Rectangle(4, 5)
+        dict_1 = rect_1.to_dictionary()
+        expected = '[{"id": 2, "width": 4, "height": 5, "x": 0, "y": 0}]'
+        self.assertEqual(Base.to_json_string([dict_1]), expected)
 
 class TestFromJSONMethod(unittest.TestCase):
-    def empty1(self):
-        empty1 = Base.from_json_string(None)
-        self.assertEqual(empty1, [])
+    def test_none_param(self):
+        presumably_empty = Base.from_json_string(None)
+        self.assertEqual(presumably_empty, [])
 
-    def empty2(self):
-        empty2 = Base.from_json_string("[]")
-        self.assertEqual(empty2, [])
+    def test_empty_param(self):
+        just_empty = Base.from_json_string("[]")
+        self.assertEqual(just_empty, [])
 
-    def dict(self):
-        d = {'id': 50}
-        dl = [d]
-        jl = Base.to_json_string(dl)
-        self.assertEqual(Base.from_json_string(jl), [{'id': 50}])
+    def test_base_dict(self):
+        base_dict = {'id': 42}
+        dict_list = [base_dict]
+        json_list = Base.to_json_string(dict_list)
+        self.assertEqual(Base.from_json_string(json_list), [{'id': 42}])
