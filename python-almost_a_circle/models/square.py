261797d5a4e6e14cf3c18fb88996d631472fbd4a
NolbsAtHolbs <hikikomode@gmail.com>
2024-01-30 21:12:48 -0600

    dictionary

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -37,3 +37,8 @@ class Square(Rectangle):
         elif kwargs:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Public method that returns dictionary representation of rectangle"""
+        return {"id": self.id, "width": self.width,
+                "height": self.height, "x": self.x, "y": self.y}
