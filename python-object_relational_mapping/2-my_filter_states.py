607f7a8da5afe2e13427f65c50eceaa701bfa53b
NolbsAtHolbs <hikikomode@gmail.com>
2024-02-20 22:01:17 -0600

    format to 4th arg

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Script that displays all values in states table where name matches arg"""
+import MySQLdb
+import sys
+
+
+def match_states():
+    dbconnect = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                                password=sys.argv[2], database=sys.argv[3])
+    dbcursor = dbconnect.cursor()
+    dbcursor.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY '{}'"
+        "ORDER BY id ASC".format(sys.arv[4]))
+    rows = dbcursor.fetchall()
+    for row in rows:
+        print(row)
+    dbcursor.close()
+    dbconnect.close()
+
+
+if __name__ == "__main__":
+    match_states()
