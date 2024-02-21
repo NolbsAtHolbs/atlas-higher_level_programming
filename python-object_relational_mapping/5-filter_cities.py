0ae662e5f567c995b5c00ba0eab14dbc9c5a5e0b
NolbsAtHolbs <hikikomode@gmail.com>
2024-02-20 23:18:27 -0600

    syntax

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -14,8 +14,7 @@ def cities_in_state():
                   ORDER BY cities.id ASC")
     dbcursor.execute(SQLcommand, (sys.argv[4],))
     rows = dbcursor.fetchall()
-    for row in rows:
-        print(row[0])
+    print(", ".join(rows[0]))
     dbcursor.close()
     dbconnect.close()
 
