bb54ce7951e1907c02bbfc6bd7b4538e207b9028
NolbsAtHolbs <hikikomode@gmail.com>
2024-02-20 21:23:41 -0600

    test

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,7 +4,8 @@ import MySQLdb
 import sys
 
 def select_states():
-    dbconnect = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], password=sys.argv[2], database=sys.argv[3])
+    dbconnect = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                                password=sys.argv[2], database=sys.argv[3])
     dbcursor = dbconnect.cursor()
     dbcursor.execute("SELECT * FROM states ORDER BY id ASC")
     rows = dbcursor.fetchall()
