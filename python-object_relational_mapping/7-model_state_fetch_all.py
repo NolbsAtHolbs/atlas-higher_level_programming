b8d99b78f833aea24716a01f7732b6a34a91582b
NolbsAtHolbs <hikikomode@gmail.com>
2024-02-20 23:45:26 -0600

    perms and pcs

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -11,7 +11,8 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    engine = create_engine(f'mysql://{username}:{password}@localhost/hbtn_0e_0_usa')
+    engine = create_engine(
+        f'mysql://{username}:{password}@localhost/hbtn_0e_0_usa')
     Session = sessionmaker(bind=engine)
     session = Session()
     rows = session.query(State).order_by(State.id).all()
