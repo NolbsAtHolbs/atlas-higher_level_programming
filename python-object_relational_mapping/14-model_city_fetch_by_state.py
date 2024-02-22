a255affcf716a95298afcbbcf6029c8bef86c5c2
NolbsAtHolbs <hikikomode@gmail.com>
2024-02-21 23:59:17 -0600

    test

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -3,7 +3,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
-from model_city import Base, City
+from model_city import City
 import sys
 
 
