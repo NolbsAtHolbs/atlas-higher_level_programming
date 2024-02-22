84ed9b455cfed0d8b1f5cd736a74a5d7ea8ccf97
NolbsAtHolbs <hikikomode@gmail.com>
2024-02-21 23:14:26 -0600

    change name with filter and recast

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Script that changes the name of a State object from the database"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+
+if __name__ == "__main__":
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    engine = create_engine(
+        f'mysql+mysqldb://{username}:{password}@localhost/{database}')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    updatestate = session.query(State).filter_by(id=2).first()
+    updatestate.name = "New Mexico"
+    session.commit()
+    session.close()
