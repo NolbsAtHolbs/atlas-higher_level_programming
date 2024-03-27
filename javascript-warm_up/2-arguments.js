4df9602aa3d9fba001d2bc8cd643f9b9349720e3
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2024-03-27 00:26:30 +0000

    cl arg processing for node with .length

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 const argsPassed = process.argv.length - 2;
 
-if (argsPassed === 0)
-  { console.log('No argument'); }
-else if (argsPassed === 1)
-  { console.log('Argument found'); }
-else
-  { console.log('Arguments found'); }
+if (argsPassed === 0) {
+    console.log("No argument");
+} else if (argsPassed === 1) {
+console.log("Argument found");
+} else {
+    console.log("Arguments found");
+}
