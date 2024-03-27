004ec055f0e800f03e2384863011b6a2b2d05996
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2024-03-27 01:06:17 +0000

    parseint test

---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+
+if (isNaN(Number(argv[2]))) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + parseInt(argv[2]));
+}
