90f8965a201848d4de1b219d9ce259cfc4f0922f
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2024-03-27 01:20:47 +0000

    forEach function element loop

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const stringArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-stringArray.forEach (function(element) {
+stringArray.forEach(function (element) {
   console.log(element);
 });
