341f060326c2b10b030c6be55ecfdd4a079e32f3
NolbsAtHolbs <144157130+NolbsAtHolbs@users.noreply.github.com>
2024-03-27 03:07:12 +0000

    recursive factorial

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 function fact (int) {
-  if (Number(int) === 0 || Number(int) === 1) {
+  if (Number(int) === 0 || Number(int) === 1 || isNaN(Number(int))) {
     return 1;
   } else {
     return (int * fact(int - 1));
