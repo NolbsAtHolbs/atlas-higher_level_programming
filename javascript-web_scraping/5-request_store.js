b8e78ecd1a5c7036dd0f6e4b9ab3ef9f65b7c3a4
NolbsAtHolbs <hikikomode@gmail.com>
2024-04-02 21:37:19 -0500

    import simplification

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -3,13 +3,13 @@ const request = require('request');
 const fs = require('fs');
 const { argv } = require('node:process');
 
-request(process.argv[2], function (err, response, body) {
+request(argv[2], function (err, response, body) {
   if (err) {
-	console.error(err);
+    console.error(err);
   }
-  fs.writeFile(process.argv[3], body, function (err) {
+  fs.writeFile(argv[3], body, function (err) {
     if (err) {
-	  console.error(err);
-	}
+      console.error(err);
+    }
   });
 });
